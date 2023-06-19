bb50b306230d2f9019eecd6130c007bcd2f329db
gecotzh <71146282+gecotzh@users.noreply.github.com>
2023-06-19 14:42:01 -0500

    Add files via upload

---
+++ Configuacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sodexo.EPedidos.Utilidades
+{
+    public class Configuacion
+    {
+        public static string GetKeyValue(string KeyName)
+        {
+            return ConfigurationManager.AppSettings[KeyName].ToString();
+        }
+
+        public static string GetConnectionValue(string ConnectionName)
+        {
+            return ConfigurationManager.ConnectionStrings[ConnectionName].ConnectionString;
+        }
+    }
+}
